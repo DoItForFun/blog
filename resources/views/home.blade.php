f0676226733f972d4836b61ace1c9b1355a73531
lizhe <Z77-2s3-Bzg-kZX>
2018-09-20 15:21:39 +0800

    1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--========== The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags ==========-->
+    <title>Home</title>
+
+    <!--==========Dependency============-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendors/owl-carousel/assets/owl.carousel.css">
+    <link rel="stylesheet" href="vendors/magnific-popup/magnific-popup.css">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit:500">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Josefin+Sans:600,700italic">
+    <link href='https://fonts.googleapis.com/css?family=Dosis:400,200,300,500,600,800,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:400,300,300italic,400italic">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500italic,500,700italic,700,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!--==========Theme Styles==========-->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/theme/green.css" rel="stylesheet">
+
+    <!--========== HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries ==========-->
+    <!--========== WARNING: Respond.js doesn't work if you view the page via file:// ==========-->
+    <!--==========[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]==========-->
+</head>
+<body class="home">
+
+<header class="row transparent black header1" data-spy="affix" data-offset-top="0" id="header">
+    <div class="container">
+        <div class="row top-header">
+            <div class="col-sm-4 search-form-col">
+                <form action="#" method="get" class="search-form">
+                    <div class="input-group">
+                        <span class="input-group-addon"><img src="images/search-icon-white.png" alt=""></span>
+                        <input type="search" class="form-control" placeholder="搜索">
+                    </div>
+                </form>
+            </div>
+            <div class="col-sm-4 logo-col text-center">
+                <a href="index.html"></a>
+            </div>
+            <div class="col-sm-4 menu-trigger-col">
+                <button class="menu-trigger pull-right">
+                    <span class="active-page">首页</span>
+                    <img src="images/menu-align-white.png" alt="" class="icon-burger">
+                    <img src="images/menu-close-white.png" alt="" class="icon-cross">
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="row menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 menu-col">
+                    <div class="row">
+                        <ul class="nav column-menu black-bg">
+                            <li class="active dropdown">
+                            <li><a href="index.html">首页</a></li>
+                            </li>
+                            <li><a href="about.html">关于</a></li>
+                            <li><a href="single.html">文章</a></li>
+
+                        </ul>
+                        <ul class="nav column-menu black-bg">
+                            <li><a href="single2.html">留言</a></li>
+
+                            <li><a href="contact.html">简历</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="row featured-post-carousel">
+    <div class="item post">
+        <img src="images/featured-posts/1.jpg" alt="" class="img-responsive main-bg">
+        <div class="post-content">
+            <div class="container">
+                <h5 class="post-meta"><i>in</i>| <a href="#">2017-08-21</a></h5>
+                <h2 class="post-title"><a href="single.html">123</a></h2>
+                <a href="single.html" class="btn btn-primary"><span>点击阅读</span></a>
+            </div>
+        </div>
+    </div>
+    <div class="item post">
+        <img src="images/featured-posts/1-1.jpg" alt="" class="img-responsive main-bg">
+        <div class="post-content">
+            <div class="container">
+                <h5 class="post-meta"><i>in</i> <a href="#">Author Profile</a> | <a href="#">feb 17, 2016</a></h5>
+                <h2 class="post-title"><a href="single.html">Nature, in the broadest sense, is the natural, physical, or material world or universe.</a></h2>
+                <a href="single.html" class="btn btn-primary"><span>read more</span></a>
+            </div>
+        </div>
+    </div>
+    <div class="item post">
+        <img src="images/featured-posts/1-2.jpg" alt="" class="img-responsive main-bg">
+        <div class="post-content">
+            <div class="container">
+                <h5 class="post-meta"><i>in</i> <a href="#">Author Profile</a> | <a href="#">feb 17, 2016</a></h5>
+                <h2 class="post-title"><a href="single.html">Nature, in the broadest sense, is the natural, physical, or material world or universe.</a></h2>
+                <a href="single.html" class="btn btn-primary"><span>read more</span></a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="row content-wrap">
+    <div class="container">
+        <div class="row" id="post-masonry">
+            <!--Blog Post-->
+            <article class="col-sm-4 post post-masonry post-format-image">
+                <div class="post-wrapper row">
+                    <div class="featured-content row">
+                        <a href="single.html"><img src="images/posts/masonry/1.jpg" alt="" class="img-responsive"></a>
+                    </div>
+                    <div class="post-excerpt row">
+                        <h5 class="post-meta">
+                            <a href="#" class="date">feb 17, 2016</a>
+                            <span class="post-author"><i>by</i><a href="#">Mark Sanders</a></span>
+                        </h5>
+                        <h3 class="post-title"><a href="single.html">Nature, in the broadest sense, is the natural, physical, or material world or universe.</a></h3>
+                        <p>In the broadest sense, is the natural, physical, or material world or universe...</p>
+                        <footer class="row">
+                            <h5 class="taxonomy"><i>in</i> <a href="#">image</a>, <a href="#">entertainment</a></h5>
+                            <div class="response-count"><img src="images/comment-icon-gray.png" alt="">5</div>
+                        </footer>
+                    </div>
+                </div>
+            </article>
+            <!--Blog Post-->
+            <article class="col-sm-4 post post-masonry post-format-image">
+                <div class="post-wrapper row">
+                    <div class="featured-content row">
+                        <a href="single.html"><img src="images/posts/masonry/2.jpg" alt="" class="img-responsive"></a>
+                    </div>
+                    <div class="post-excerpt row">
+                        <h5 class="post-meta">
+                            <a href="#" class="date">feb 17, 2016</a>
+                            <span class="post-author"><i>by</i><a href="#">Mark Sanders</a></span>
+                        </h5>
+                        <h3 class="post-title"><a href="single.html">Nature, in the broadest sense, is the natural, physical, or material world or universe.</a></h3>
+                        <p>In the broadest sense, is the natural, physical, or material world or universe...</p>
+                        <footer class="row">
+                            <h5 class="taxonomy"><i>in</i> <a href="#">image</a>, <a href="#">entertainment</a></h5>
+                            <div class="response-count"><img src="images/comment-icon-gray.png" alt="">5</div>
+                        </footer>
+                    </div>
+                </div>
+            </article>
+
+            <article class="col-sm-4 post post-masonry post-format-text">
+                <div class="post-wrapper row">
+                    <div class="post-excerpt row">
+                        <h5 class="post-meta">
+                            <a href="#" class="date">feb 17, 2016</a>
+                            <span class="post-author"><i>by</i><a href="#">Mark Sanders</a></span>
+                        </h5>
+                        <h3 class="post-title"><a href="single.html">Nature, in the broadest sense, is the natural, physical, or material world or universe.</a></h3>
+                        <p>In the broadest sense, is the natural, physical, or material world or universe...</p>
+                        <footer class="row">
+                            <h5 class="taxonomy"><i>in</i> <a href="#">image</a>, <a href="#">entertainment</a></h5>
+                            <div class="response-count"><img src="images/comment-icon-gray.png" alt="">5</div>
+                        </footer>
+                    </div>
+                </div>
+            </article>
+            <!--Blog Post-->
+            <article class="col-sm-4 post post-masonry post-format-video">
+                <div class="post-wrapper row">
+                    <div class="featured-content row">
+                        <a href="single.html">
+                            <img src="images/posts/masonry/4.jpg" alt="" class="img-responsive">
+                            <img src="images/play-btn.png" alt="" class="video-mark">
+                        </a>
+                    </div>
+                    <div class="post-excerpt row">
+                        <h5 class="post-meta">
+                            <a href="#" class="date">feb 17, 2016</a>
+                            <span class="post-author"><i>by</i><a href="#">Mark Sanders</a></span>
+                        </h5>
+                        <h3 class="post-title"><a href="single.html">Nature, in the broadest sense, is the natural, physical, or material world or universe.</a></h3>
+                        <p>In the broadest sense, is the natural, physical, or material world or universe...</p>
+                        <footer class="row">
+                            <h5 class="taxonomy"><i>in</i> <a href="#">image</a>, <a href="#">entertainment</a></h5>
+                            <div class="response-count"><img src="images/comment-icon-gray.png" alt="">5</div>
+                        </footer>
+                    </div>
+                </div>
+            </article>
+
+        </div>
+    </div>
+</section>
+
+<!--Footer-->
+<footer class="row" id="footer">
+    <div class="container">
+
+        <h5 class="copyright">Copyright &copy; 2017.Company name All rights reserved</h5>
+    </div>
+</footer>
+
+<!--========== jQuery (necessary for Bootstrap's JavaScript plugins) ==========-->
+<script src="js/jquery-2.2.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+<script src="vendors/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="vendors/instafeed/instafeed.min.js"></script>
+<script src="vendors/imagesLoaded/imagesloaded.pkgd.min.js"></script>
+<script src="vendors/isotope/isotope.pkgd.min.js"></script>
+<script src="js/theme.js"></script>
+</body>
+</html>
