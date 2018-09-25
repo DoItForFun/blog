133e6735c984a333c63bb0ed367ca547f0ada8d6
DoItForFun <287197192@qq.com>
2018-09-25 23:08:16 +0800

    a

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 define('APPLICATION_PATH', dirname(__FILE__));
 define('APP_ROOT', dirname(__FILE__) );
 $application = new Yaf\Application( APPLICATION_PATH . "/conf/application.ini",\Yaf\ENVIRON);
