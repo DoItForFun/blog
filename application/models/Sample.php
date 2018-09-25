85515bcd6637918a6e3cdce967837f8f848b7d25
DoItForFun <287197192@qq.com>
2018-09-25 21:36:03 +0800

    1

--- application/models/Sample.php
+++ application/models/Sample.php
@@ -2,7 +2,7 @@
 /**
  * @name SampleModel
  * @desc sample数据获取类, 可以访问数据库，文件，其它系统等
- * @author lizhe
+ * @author root
  */
 class SampleModel {
     public function __construct() {
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 define('APPLICATION_PATH', dirname(__FILE__));
-define('APP_ROOT', dirname(__FILE__) );
-$application = new Yaf\Application( APPLICATION_PATH . "/conf/application.ini",\Yaf\ENVIRON);
-//$application->getDispatcher()->returnResponse(true)->getApplication()->bootstrap()->run();
+
+$application = new Yaf\Application( APPLICATION_PATH . "/conf/application.ini");
+
 $application->bootstrap()->run();
+?>
